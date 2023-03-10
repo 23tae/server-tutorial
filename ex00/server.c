8e2d62143af01ee2278315d51788b3a4085707d1
Taehoon Kim <taehoonkim.dev@gmail.com>
2023-03-10 07:29:58 +0000

    chore: change function name

--- ex00/server.c
+++ ex00/server.c
@@ -7,8 +7,8 @@
 
 void	print_error(char *message)
 {
-	fputs(message, stderr);
-	fputc('\n', stderr);
+	write(2, message, strlen(message));
+	write(2, "\n", 1);
 	exit(1);
 }
 
@@ -32,53 +32,55 @@ void	init_server_addr(struct sockaddr_in *server_addr, char *port)
  * @param port port number
  * @return int socket descriptor
  */
-int	init_socket(char *port)
+int	init_server_socket(char *port)
 {
-	int	server_sock;
+	int	server_socket;
 	struct sockaddr_in server_addr;
 
-	server_sock = socket(PF_INET, SOCK_STREAM, 0);
-	if (server_sock == -1)
-		print_error("Error occured while socket");
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_socket == -1)
+		print_error("Error occured in socket");
 	init_server_addr(&server_addr, port);
 
 	// Bind socket and server IP address
-	if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
-		print_error("Error occured while bind");
+	if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+		print_error("Error occured in bind");
 
 	// Create connection request queue as maximum number 5
-	if (listen(server_sock, 5) == -1)
-		print_error("Error occured while listen");
-	return (server_sock);
+	if (listen(server_socket, 5) == -1)
+		print_error("Error occured in listen");
+	return (server_socket);
 }
 
-int	socket_connection(int server_sock)
+int	connect_socket(int server_socket)
 {
-	int client_sock;
+	int client_socket;
+	char *message;
 	struct sockaddr_in client_addr;
 	socklen_t client_addr_size;
 
 	client_addr_size = sizeof(client_addr);
-	client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_addr_size);
-	if (client_sock == -1)
-		print_error("Error occured while accept");
-
-	char msg[] = "Server Connected!\n";
-	write(client_sock, msg, sizeof(msg));
+	client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_size);
+	if (client_socket == -1)
+		print_error("Error occured in accept");
+	printf("Found client!\n");
+	message = "Server connected!\n";
+	write(client_socket, message, strlen(message));
 }
 
 int	main(int argc, char **argv)
 {
-	int	server_sock;
-	int	client_sock;
+	int	server_socket;
+	int	client_socket;
 
 	if (argc != 2)
 		print_error("Usage : ./server <port number>");
 
-	server_sock = init_socket(argv[1]);
-	client_sock = socket_connection(server_sock);
+	server_socket = init_server_socket(argv[1]);
+	client_socket = connect_socket(server_socket);
 
-	close(client_sock);
-	close(server_sock);
+	printf("Close connection.\n");
+	close(client_socket);
+	close(server_socket);
 	return (0);
 }
